aa1fce791e404fc178b974ba165bbe1bcd0a6737
Oren Gampel <gampel@epiphyte.com>
2018-10-14 15:11:42 +0300

    change green icon to use https://github.com/pointhi/leaflet-color-markers (as another option)

--- js/main.js
+++ js/main.js
@@ -2042,12 +2042,14 @@ function showPackageDetails(escrow_pubkey){
     iconAnchor: [22, 37],
     popupAnchor: [-3, -38],
     })
-    var greenIcon = L.icon({
-    iconUrl: 'green_location.png',
-    iconSize: [38, 38],
-    iconAnchor: [22, 37],
-    popupAnchor: [-3, -38],
-    })
+    var greenIcon = new L.Icon({
+      iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
 
 
     requests.router
